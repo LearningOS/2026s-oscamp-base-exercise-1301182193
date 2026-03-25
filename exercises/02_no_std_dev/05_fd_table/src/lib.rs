b36fcef13522006261c9e7e1e20e8d0dbb2d910f
1301182193 <1301182193@qq.com>
2026-03-25 14:22:55 +0800

    update

--- exercises/02_no_std_dev/05_fd_table/src/lib.rs
+++ exercises/02_no_std_dev/05_fd_table/src/lib.rs
@@ -48,6 +48,7 @@ pub trait File: Send + Sync {
 
 /// File descriptor table
 pub struct FdTable {
+    fdtable: Vec<Option<Arc<dyn File>>>,
     // TODO: Design the internal structure
     // Hint: use Vec<Option<Arc<dyn File>>>
     //       the index is the fd number, None means the fd is closed or unallocated
@@ -57,7 +58,9 @@ impl FdTable {
     /// Create an empty fd table
     pub fn new() -> Self {
         // TODO
-        todo!()
+        FdTable {
+            fdtable: Vec::new(),
+        }
     }
 
     /// Allocate a new fd, return the fd number.
@@ -65,25 +68,40 @@ impl FdTable {
     /// Prefers reusing the smallest closed fd number; if no free slot, appends to the end.
     pub fn alloc(&mut self, file: Arc<dyn File>) -> usize {
         // TODO
-        todo!()
+        for (i, slot) in self.fdtable.iter_mut().enumerate() {
+            if slot.is_none() {
+                *slot = Some(file);
+                return i;
+            }
+        }
+
+        self.fdtable.push(Some(file));
+        self.fdtable.len() - 1
     }
 
     /// Get the file object for an fd. Returns None if the fd doesn't exist or is closed.
     pub fn get(&self, fd: usize) -> Option<Arc<dyn File>> {
         // TODO
-        todo!()
+        self.fdtable
+            .get(fd)
+            .and_then(|opt| opt.as_ref().cloned())
     }
 
     /// Close an fd. Returns true on success, false if the fd doesn't exist or is already closed.
     pub fn close(&mut self, fd: usize) -> bool {
-        // TODO
-        todo!()
+        match self.fdtable.get_mut(fd) {
+            Some(slot) if slot.is_some() => {
+                *slot = None;
+                true
+            }
+            _ => false,
+        }
     }
 
     /// Return the number of currently allocated fds (excluding closed ones)
     pub fn count(&self) -> usize {
         // TODO
-        todo!()
+        self.fdtable.iter().filter(|f| f.is_some()).count()
     }
 }
 
