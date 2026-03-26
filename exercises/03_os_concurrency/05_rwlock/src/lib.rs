76b325bf9e11dd98ad2a6d96c50d479c19b64532
1301182193 <1301182193@qq.com>
2026-03-27 02:31:04 +0800

    123

--- exercises/03_os_concurrency/05_rwlock/src/lib.rs
+++ exercises/03_os_concurrency/05_rwlock/src/lib.rs
@@ -41,10 +41,20 @@ const WRITER_WAITING: u32 = 1 << 31;
 pub struct RwLock<T> {
     state: AtomicU32,
     data: UnsafeCell<T>,
+    //UnsafeCell 是唯一允许“通过共享引用 &T 修改数据”的类型，
+    //  即不再需要取得这个数据的可变引用，通过 data.get()即可得到其可变裸指针(*mut T)
+    //  self.data.get() -> *mut T
+    //  绕过了 rust 的核心保证：“&T 不可修改”，允许&self 修改内部数据
+    //  它是所有并发原语的基础
 }
 
 unsafe impl<T: Send> Send for RwLock<T> {}
+//<T: Send>: 这个是要求使用 RwLock 的类型，一定要实现了 Send
+// 对于Send: 自己实现的数据类型，需要实现 Send trait，才能够被移动进新的线程，即在新线程中能够使用这个数据类型
 unsafe impl<T: Send + Sync> Sync for RwLock<T> {}
+// 对于Sync: 自己实现的数据类型，需要实现 Sync trait， 新的数据类型才能够在多个线程间共享
+//<T: Send + Sync>: 这个是要求使用 RwLock 的类型，一定要实现了 Send + Sync
+
 
 impl<T> RwLock<T> {
     pub const fn new(data: T) -> Self {
@@ -67,6 +77,7 @@ impl<T> RwLock<T> {
             let t: u32 = self.state.load(Ordering::Acquire) as u32;
             if t & (WRITER_HOLDING | WRITER_WAITING) == 0 && (t & READER_MASK != READER_MASK) {
                 match self.state.compare_exchange(t, t + 1, Ordering::AcqRel, Ordering::Acquire) {
+                    //          对于 compare_exchange:成功一定要使用 Order::AcqRel,因为要写这个变量，首先需要保证读的正确性，然后保证写的正确性
                     Ok(_) => {
                         return RwLockReadGuard {
                             lock: self
@@ -76,6 +87,8 @@ impl<T> RwLock<T> {
                 }
             } else {
                 core::hint::spin_loop();
+                //hint 给 CPU 的提示，
+                //spin_loop 提示 CPU: 当前线程在忙等待（spin），可以优化（例如 pause 指令）
             }
         }
     }
@@ -94,16 +107,16 @@ impl<T> RwLock<T> {
             let s = self.state.load(Ordering::Acquire);
             if (s & READER_MASK) != 0 || (s & WRITER_HOLDING) != 0{
                 core::hint::spin_loop();
-                continue;
-            }
-            match self.state.compare_exchange(s, WRITER_HOLDING, Ordering::AcqRel, Ordering::Acquire) {
-                Ok(_) => {
-                    return RwLockWriteGuard {
-                        lock: self
+            } else {
+                match self.state.compare_exchange(s, WRITER_HOLDING, Ordering::AcqRel, Ordering::Acquire) {
+                    Ok(_) => {
+                        return RwLockWriteGuard {
+                            lock: self
+                        }
+                    },
+                    Err(_) => {
+                        continue
                     }
-                },
-                Err(_) => {
-                    continue
                 }
             }
         }
@@ -113,6 +126,17 @@ impl<T> RwLock<T> {
 /// Guard for a read lock; releases the read lock on drop.
 pub struct RwLockReadGuard<'a, T> {
     lock: &'a RwLock<T>,
+    //这里的 lock 是 RwLock<T> 的引用类型，所以这里是借用
+    //因为这是一个引用，引用的生命周期一定要 >= guard 的，不然就会造成悬垂引用
+    //‘an 表示这个引用最多就活到 ‘a，如果超过这个生命周期，编译器报错
+    //     let guard;
+    // {
+    //     let lock = RwLock::new(5);
+    //     guard = lock.read(); // guard 借用了 lock
+    // }  lock 在这里被释放
+    // ❌ 这里还在用 guard
+    // println!("{}", *guard);
+    //编译器报错：borrowed value does not live long enough
 }
 
 // TODO: Implement Deref for RwLockReadGuard
@@ -149,8 +173,20 @@ impl<T> Deref for RwLockWriteGuard<'_, T> {
         unsafe {
             &*self.lock.data.get()
         }
+        //使用 unsafe 的原因：因为self.lock.data.get()返回的是裸指针类型 *mut T
+        //  将裸指针转换成引用在 rust 中是不安全的操作，因此需要使用 unsafe 来避免 rust 的检查
+
+        //  使用 &* 的原因：先理解 *, 再理解 & 
+        //  使用 * 的原因：将裸指针ptr: *mut T 转换为 *ptr: T,注意：这里并没有转移其所有权，只是指向了这个位置
+        //  使用 & 的原因： 对 （*ptr）中的值取引用
+        //  最终的结果是返回了 UnsafeCell<T> 里面的 T 的引用，因为这个 data 是 UnsafeCell 类型的，可以直接通过 &T（引用) 修改
     }
 }
+
+//实现 Deref 和 DerefMut 这两个 trait 之后，如果需要访问这个引用的值，直接使用 *lock 即可，
+//  如果没有实现 Deref，查看 lock 的数据的时候，需要 unsafe{ &*lock.data.get() },很麻烦
+
+//对于 Deref 和 DerefMut 来说，需要修改的时候，自动会调用 DerefMut 以供修改，只读的时候调用 Deref
 
 // TODO: Implement DerefMut for RwLockWriteGuard
 // Return mutable reference: unsafe { &mut *self.lock.data.get() }
